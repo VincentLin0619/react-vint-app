80e2712021b517faaca15fc99c0836913b8a3067
Vincentlin0619 <vincentlin19910619@gmail.com>
2022-12-05 16:36:53 +0800

    npm i react-date-range import format from 'date-fns/format';（讓日期能以區間方式呈現） {format(dates[0].startDate, "MM/dd/yy")} -           {format(dates[0].endDate, "MM/dd/yy")}

--- src/components/NavBar/SearchBar.jsx
+++ src/components/NavBar/SearchBar.jsx
@@ -7,9 +7,16 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./SearchBar.scss";
 import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRange } from "react-date-range";
+import * as locales from "react-date-range/dist/locale";
+import format from "date-fns/format";
 
 export const SearchBar = () => {
-  const [destination, setDestination] = useState("");
+  const [dates, setDates] = useState([
+    { startDate: new Date(), endDate: new Date(), key: "selection" },
+  ]);
   const [openCalendar, setOpenCalendar] = useState(false);
   return (
     <div className="headerSearchBar">
@@ -22,17 +29,37 @@ export const SearchBar = () => {
         />
       </div>
       <div className="searchBarItem">
-        <FontAwesomeIcon icon={faCalendar} />
-        <span className="searchText">12/12/2022-12/12/2023</span>
-      </div>
-      <div className="searchBarItem">
-        <FontAwesomeIcon icon={faPeopleGroup} />
+        <FontAwesomeIcon
+          icon={faCalendar}
+          onClick={() => setOpenCalendar(!openCalendar)}
+        />
+        {/* {openCalendar ? false : <DateRange />} */}
+        {openCalendar && (
+          <DateRange
+            editableDateInputs={true} //可以讓日期被選取並輸入等等的
+            onChange={(item) => setDates([item.selection])}
+            //onChange把紀錄到的改動都紀錄到state date 裡面我們暫存器就會有選好的日期範圍，等於是輸入到暫存器
+            //item.selection的概念就是讓他選擇上傳到key=selection的部分，因為
+            moveRangeOnFirstSelection={false}
+            className="calendar" //並記得classname scss styling導入
+            minDate={new Date()}
+            ranges={dates} //可以選範圍並範圍更改會re-render useState的date等於這是個抓取date範圍並顯示在日曆上，等於是從暫存器輸入到日曆顯示上面
+            locale={locales["zhTW"]}
+            //最後這邊就是語言版本使用繁體中文zhTW概念
+            //就可以用到上面的import * as locales from 'react-date-range/dist/locale';
+          />
+        )}
         <span
           className="searchText"
           onClick={() => setOpenCalendar(!openCalendar)}
         >
-          3位成人，兩位小孩，1間房
+          {format(dates[0].startDate, "MM/dd/yy")} -{" "}
+          {format(dates[0].endDate, "MM/dd/yy")}
         </span>
+      </div>
+      <div className="searchBarItem">
+        <FontAwesomeIcon icon={faPeopleGroup} />
+        <span className="searchText">3位成人，兩位小孩，1間房</span>
       </div>
       <button className="searchBarBtn">搜尋</button>
     </div>
