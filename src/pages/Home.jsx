22071cbf4199c3e64b82c823c8c814173147e066
Vincentlin0619 <vincentlin19910619@gmail.com>
2022-12-06 15:24:07 +0800

    刻出HomePage的資訊區塊（announcementContainer）

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,36 @@ import React from "react";
 import MainMenu from "../components/NavBar/MainMenu";
 import SubMenu from "../components/NavBar/SubMenu";
 import "./Home.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   return (
     <div>
       <MainMenu />
       <SubMenu />
+      <div className="announcementContainer">
+        <div className="checkContainer">
+          <input type="checkbox" name="checkbox" id="checkbox" />
+          此為出差行程
+        </div>
+        <div className="infoContainer">
+          <FontAwesomeIcon icon={faInfoCircle} />
+          獲得所需建議。在出發之前，查看最新的新冠肺炎（COVID-19）相關限制。瞭解更多
+        </div>
+        <div className="discountContainer">
+          <div className="left">
+            <img src="" alt="" />
+          </div>
+          <div className="right">
+            <h2>省15%或更多</h2>
+            <span>
+              這個夏天，讓夢想之旅成真｜2022年09月30日前預訂並住房就可省一筆
+            </span>
+            <button>逛逛優惠</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
