9ef4170c1a7cfd5df50693175b4304f1b0bedc12
Vincentlin0619 <vincentlin19910619@gmail.com>
2022-12-21 11:04:08 +0800

    把line-two的選單變成漢堡

--- src/components/NavBar/MainMenu.jsx
+++ src/components/NavBar/MainMenu.jsx
@@ -25,26 +25,43 @@ const MainMenu = () => {
           </div>
         </div>
         <div className="lineTwo">
-          <div className="item active">
-            <FontAwesomeIcon icon={faBed} />
-            <span>住宿</span>
-          </div>
-          <div className="item">
-            <FontAwesomeIcon icon={faPlane} />
-            <span>航班</span>
-          </div>
-          <div className="item">
-            <FontAwesomeIcon icon={faCar} />
-            <span>租車</span>
-          </div>
-          <div className="item">
-            <FontAwesomeIcon icon={faToriiGate} />
-            <span>景點/活動</span>
-          </div>
-          <div className="item">
-            <FontAwesomeIcon icon={faTaxi} />
-            <span>機場計程車</span>
-          </div>
+          <input type="checkbox" id="nav-toggle" />
+          <label htmlFor="nav-toggle" className="ham">
+            <span className="ham-origin">
+              {/* 塞三條線 */}
+              <span className="ham-bar ham-bar-1"></span>
+              <span className="ham-bar ham-bar-2"></span>
+              <span className="ham-bar ham-bar-3"></span>
+            </span>
+          </label>
+          <nav className="menu">
+            <ul className="menu-list">
+              <li className="menu-item">
+                <a href="#" className="menu-link">
+                  <FontAwesomeIcon icon={faBed} />
+                  <span>住宿</span>
+                </a>
+              </li>
+              <li className="menu-item">
+                <a href="#" className="menu-link">
+                  <FontAwesomeIcon icon={faCar} />
+                  <span>租車</span>
+                </a>
+              </li>
+              <li className="menu-item">
+                <a href="#" className="menu-link">
+                  <FontAwesomeIcon icon={faToriiGate} />
+                  <span>景點/活動</span>
+                </a>
+              </li>
+              <li className="menu-item">
+                <a href="#" className="menu-link">
+                  <FontAwesomeIcon icon={faTaxi} />
+                  <span>機場計程車</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </div>
